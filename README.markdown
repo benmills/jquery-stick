832233b65e6c18378d339c130a9f13cc8a9b70f9
benmills <ben@getbraintree.com>
2011-05-10 23:02:31 -0500

    Slight code style and readme update

--- README.markdown
+++ README.markdown
@@ -4,6 +4,34 @@ Stick is a simple plugin that will make an element stick in place as you scroll
 
     $('#menu').stick();
 
+## Options
+
+There are only a few options:
+
+* offset: the distance from the top of the screen the element should be.
+* onStick: a callback for when the element sticks.
+* onUnstick: a callback when the element stops being sticky.
+
+You can set the options by passing in an object:
+
+```javascript
+$("#menu").stick({
+  offset: 10,
+  onStick: function () {},
+  onUnstick: function () {}
+});
+```
+
+As a shortcut, you can pass an integer in and jquery-stick will use that as the offset value:
+
+```javascript
+$("#menu").stick(10);
+```
+
+## Examples
+
+This plugin was made when I couldn't find a simple enough existing jquery plugin while I was building the latest version of [Braintree's documentation site](http://www.braintreepayments.com/docs/ruby). In this site I'm sticking two different elements, the sidebar menu and the top menu.
+
 ## License
 
 Copyright (c) 2011 Ben Mills
--- jquery.stick.js
+++ jquery.stick.js
@@ -6,35 +6,35 @@
       offset: 0,
       target: null,
       targetOffset: null,
-      staticCallback: null,
-      fixedCallback: null,
-    }
+      onUnstick: null,
+      onStick: null,
+    };
 
     // Populate Options
     if (typeof args == "number") {
-      options.offset = args  
+      options.offset = args;
     } else if (typeof args == "object") {
-      options = $.extend(options, args)   
+      options = $.extend(options, args);
     }
 
     // Private Methods
 
     var _resize = function(){
-      if (options.state == options.states.fixed) _position_static()
-      options.targetOffset = parseInt(options.target.offset().top)
-      options.targetLeft = parseInt(options.target.offset().left)
+      if (options.state == options.states.fixed) _position_static();
+      options.targetOffset = parseInt(options.target.offset().top);
+      options.targetLeft = parseInt(options.target.offset().left);
 
-      if (options.state == options.states.fixed) _position_fixed()
+      if (options.state == options.states.fixed) _position_fixed();
 
       // Check to see if the window is too small for the sticked objectf
-      var targetBottom = options.targetOffset+options.target.height()
+      var targetBottom = options.targetOffset+options.target.height();
 
       if($(window).height() < targetBottom) {
-        options.disable = true
-        if (options.state == options.states.fixed) _position_static()
+        options.disable = true;
+        if (options.state == options.states.fixed) _position_static();
       } else {
-        options.disable = false
-        _scroll()
+        options.disable = false;
+        _scroll();
       }
     }
 
@@ -43,45 +43,44 @@
         "position":"fixed",
         "top": options.offset+"px",
         "left": options.targetLeft+"px",
-      })
+      });
     }
-    
+
     var _position_static = function() {
       options.target.css({
         "position":"static",
         "top": ""
-      })
+      });
     }
 
     var _scroll = function() {
       if (options.disable) return;
 
       if ((options.targetOffset - $(this).scrollTop()) - options.offset <= 0) {
-        if (options.state == options.states.static && options.fixedCallback) options.fixedCallback()
+        if (options.state == options.states.static && options.onStick) options.onStick()
 
-        _position_fixed()
-        options.state = options.states.fixed
+        _position_fixed();
+        options.state = options.states.fixed;
       } else {
-        if (options.state == options.states.fixed && options.staticCallback) 
-          options.staticCallback()
+        if (options.state == options.states.fixed && options.onUnstick) options.onUnstick();
 
-        _position_static()
-        options.state = options.states.static
+        _position_static();
+        options.state = options.states.static;
       }
     }
 
     // Private Options
 
-    options.target = this
+    options.target = this;
     options.states = {
       "static": "static",
       "fixed": "fixed"
-    }
-    options.state = options.states.static
+    };
+    options.state = options.states.static;
 
     // Init
 
-    _resize()
-    $(window).scroll(_scroll).resize(_resize)
+    _resize();
+    $(window).scroll(_scroll).resize(_resize);
   }
-})(jQuery)
+})(jQuery);
